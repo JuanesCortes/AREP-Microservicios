979551d59afb26f26a92acce0926019261ab60fc
DDRBernal <otalorabernal@gmail.com>
2022-11-15 21:45:36 -0500

    login added

--- src/main/resources/files/js/app.js
+++ src/main/resources/files/js/app.js
@@ -1,61 +1,72 @@
-var app = (function () {
-  var view = function(error, res) {
-      $("#ok").text(res.confirm);
-      return "callback";
+var app = (function() {
+
+    var view = function(error, res) {
+        $("#ok").text(res.confirm);
+        return "callback";
     }
 
-  function addWordTyped(word) {
-      apiclient.addWord(word);
-  }
+    function addWordTyped(word) {
+        apiclient.addWord(word);
+    }
 
-  function showWords(word){
-    apiclient.showWords((req, resp) => {
-        createDataTable(resp);
-    });
-  }
+    function showWords(word) {
+        apiclient.showWords((req, resp) => {
+            createDataTable(resp);
+        });
+    }
 
-  function createDataTable(data){
-    let table = $("#fl-table tbody");
-    table.empty();
-    if (data !== undefined) {
-      console.log(data);
-      //datanew = data.split(",");
-      var temp_json = JSON.parse(data);
-//      datanew = datanew.slice(Math.max(datanew.length - 10, 0))
-      //let date = getCurrentDay();
-      temp_json.forEach(function parsejson(item_tweet) {
-        let values = item_tweet;
-        let username = values.username;
-        let tweet = values.mensaje;
-        let date = values.fecha;
+    function createDataTable(data) {
+        let table = $("#fl-table tbody");
+        table.empty();
+        if (data !== undefined) {
+            console.log(data);
 
-        document.getElementById("info").innerHTML +=
-        `<tr>
+            var temp_json = JSON.parse(data);
+
+            temp_json.forEach(function parsejson(item_tweet) {
+                let values = item_tweet;
+                let username = values.username;
+                let tweet = values.mensaje;
+                let date = values.fecha;
+
+                document.getElementById("info").innerHTML +=
+                    `<tr>
             <td>${username}</td>
             <td>${tweet}</td>
             <td>${date}</td>
         </tr>`
-      });
+            });
 
 
 
-    } else {
-        alert("Data not found!");
+        } else {
+            alert("Data not found!");
+        }
     }
-  }
 
-  function getCurrentDay(){
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0');
-    var yyyy = today.getFullYear();
-    today = dd + '/' + mm + '/' + yyyy;
-    return today;
-  }
+    function login(user, pswd) {
+        alert(user+" "+pswd);
+//        apiclient.login(user, pswd, (req, resp) => {
+//            alert(req+" "+resp);
+//            console.log(resp);
+//        });
+    }
 
-  return {
-	  addWordTyped:addWordTyped,
-	  showWords: showWords
-  }
+    function getCurrentDay() {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0');
+        var yyyy = today.getFullYear();
+        today = dd + '/' + mm + '/' + yyyy;
+        return today;
+    }
+
+
+
+    return {
+        login: login,
+        addWordTyped: addWordTyped,
+        showWords: showWords
+    }
 
 })();
