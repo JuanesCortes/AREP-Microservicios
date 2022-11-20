ff39f8fedb7694127bb10d1669269f902318915e
JuanesCortes <juanes9027@gmail.com>
2022-11-20 17:18:10 -0500

    Nombre de suario en el post

--- src/main/java/edu/eci/arep/arepmicroservicios/model/makeTweet.java
+++ src/main/java/edu/eci/arep/arepmicroservicios/model/makeTweet.java
@@ -1,15 +1,19 @@
-
 package edu.eci.arep.arepmicroservicios.model;
+
+import com.google.gson.Gson;
+import java.util.Properties;
 
 /**
  *
  * @author juane
  */
 public class makeTweet {
+
     public static Tweet createTweet(String json) {
-        System.out.println(json);
-        String user = "unknown";//json.replace("{\"username\":", "").replace("}", "");
-        String tweetMessage = json.replace("{\"tweet\":", "").replace("}", "");
+        final Gson gson = new Gson();
+        final Properties properties = gson.fromJson(json, Properties.class);
+        String user = properties.getProperty("username");
+        String tweetMessage = properties.getProperty("tweet");
         Tweet tweet = new Tweet(tweetMessage, java.time.LocalDate.now().toString(), user);
         return tweet;
     }
